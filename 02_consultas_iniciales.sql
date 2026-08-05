b9465445616bf189a2488d0d6e04edda7afbb20e
carloscisterna91 <102269014+carloscisterna91@users.noreply.github.com>
2026-08-05 13:25:22 -0300

    Delete 02_consultas_iniciales.sql

--- 02_consultas_iniciales.sql
+++
@@ -1,8 +0,0 @@
-Use CarlosCisterna
-
-select * from Clientes 
-select * from productos
-select * from vendedores
-select * from ventas
-
-select id_vendedor, SUM(total)importe from ventas group by id_vendedor order by importe desc
